7f879cbc22fb7b84fd12f87b18eb55587c62a9af
Grant Orchard <go@hashicorp.com>
2020-01-28 15:27:44 +1100

    Testing T0 deployment

--- main.tf
+++ main.tf
@@ -2,6 +2,18 @@ provider nsxt {
   allow_unverified_ssl = var.allow_unverified_ssl
 }
 
-data nsxt_edge_cluster "this" {
+data nsxt_edge_cluster "prod" {
   display_name = var.edge_cluster_name
+}
+
+resource "nsxt_logical_tier0_router" "prod" {
+  display_name           = "prod-t0"
+  description            = "Production T0 router"
+  high_availability_mode = "ACTIVE_STANDBY"
+  edge_cluster_id        = data.nsxt_edge_cluster.prod.id
+
+  tag {
+    environment = "production"
+    managed_by   = "Terraform Enterprise"
+  }
 }
