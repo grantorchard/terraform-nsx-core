dd8c70e9e20aeed2bbff922e251b78afb68b7ac8
Grant Orchard <go@hashicorp.com>
2020-01-28 15:44:16 +1100

    Added bits to try and create a T1 router connected to the existing T0

--- main.tf
+++ main.tf
@@ -6,9 +6,32 @@ data nsxt_edge_cluster "prod" {
   display_name = var.edge_cluster_name
 }
 
-resource "nsxt_logical_tier0_router" "prod" {
-  display_name           = "prod-t0"
-  description            = "Production T0 router"
-  high_availability_mode = "ACTIVE_STANDBY"
-  edge_cluster_id        = data.nsxt_edge_cluster.prod.id
+data nsxt_logical_tier0_router "prod" {
+  display_name = "t0-router"
+}
+
+resource nsxt_logical_router_link_port_on_tier0 "prod_web" {
+  description       = ""
+  display_name      = "prod_web"
+  logical_router_id = data.nsxt_logical_tier0_router.prod.id
+}
+
+resource nsxt_logical_router_link_port_on_tier1 "prod_web" {
+  description                   = ""
+  display_name                  = "prod_web"
+  logical_router_id             = nsxt_logical_tier1_router.prod_web.id
+  linked_logical_router_port_id = nsxt_logical_router_link_port_on_tier0.prod_web.id
+}
+
+resource nsxt_logical_tier1_router "prod_web" {
+  description                 = "Production Web"
+  display_name                = "prod_web"
+  failover_mode               = "PREEMPTIVE"
+  edge_cluster_id             = data.nsxt_edge_cluster.prod.id
+  enable_router_advertisement = true
+  advertise_connected_routes  = false
+  advertise_static_routes     = true
+  advertise_nat_routes        = true
+  advertise_lb_vip_routes     = true
+  advertise_lb_snat_ip_routes = false
 }
