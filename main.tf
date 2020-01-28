de61186f1601b7ddf074ae0fd59b331ed5b5c929
Grant Orchard <go@hashicorp.com>
2020-01-28 15:06:41 +1100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,3 @@
+data nsxt_edge_cluster "edge_cluster1" {
+  display_name = var.edge_cluster_name
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable edge_cluster_name {
+  type = string
+}
+
+variable t0_name {
+  type = string
+}
