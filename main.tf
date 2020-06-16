c335e0d40f282a0e7c9911d6f9a638d806518887
Grant Orchard <go@hashicorp.com>
2020-06-16 16:49:31 +1000

    added tz path as an output, and tagged the tier0 gateway

--- main.tf
+++ main.tf
@@ -33,6 +33,11 @@ resource nsxt_policy_tier0_gateway "this" {
   internal_transit_subnets  = ["102.64.0.0/16"]
   transit_subnets           = ["101.64.0.0/16"]
   edge_cluster_path         = data.nsxt_policy_edge_cluster.this.path
+  
+  tag {
+    scope = "terraform managed"
+    tag   = "true"
+  }
 }
 
 
@@ -61,75 +66,3 @@ resource nsxt_policy_static_route "this" {
     tag   = "true"
   }
 }
-/*
-resource nsxt_policy_tier1_gateway "this" {
-  description = var.description
-  display_name              = "Tier1-gw1"
-  nsx_id                    = "predefined_id"
-  edge_cluster_path         = data.nsxt_policy_edge_cluster.this.path
-  failover_mode             = "PREEMPTIVE"
-  default_rule_logging      = "false"
-  enable_firewall           = "true"
-  enable_standby_relocation = "false"
-  force_whitelisting        = "true"
-  tier0_path                = nsxt_policy_tier0_gateway.this.path
-  route_advertisement_types = [
-    "TIER1_STATIC_ROUTES",
-    "TIER1_CONNECTED"
-  ]
-  pool_allocation           = "ROUTING"
-
-  tag {
-    scope = "color"
-    tag   = "blue"
-  }
-
-  route_advertisement_rule {
-    name                      = "rule1"
-    action                    = "DENY"
-    subnets                   = ["20.0.0.0/24", "21.0.0.0/24"]
-    prefix_operator           = "GE"
-    route_advertisement_types = ["TIER1_CONNECTED"]
-  }
-}
-
-resource nsxt_policy_tier1_gateway_interface "this" {
-  display_name           = "segment1_interface"
-  description = var.description
-  gateway_path           = data.nsxt_policy_tier1_gateway.gw1.path
-  segment_path           = nsxt_policy_vlan_segment.segment1.path
-  subnets                = ["12.12.2.13/24"]
-  mtu                    = 1500
-}
-
-resource nsxt_policy_segment "this" {
-    display_name        = "segment1"
-    description = var.description
-    connectivity_path   = nsxt_policy_tier1_gateway.this.path
-    transport_zone_path = data.nsxt_policy_transport_zone.overlay.path
-
-    subnet {
-      cidr        = "12.12.2.1/24"
-      dhcp_ranges = ["12.12.2.100-12.12.2.160"]
-
-      dhcp_v4_config {
-        server_address = "12.12.2.2/24"
-        lease_time     = 36000
-
-        dhcp_option_121 {
-          network  = "6.6.6.0/24"
-          next_hop = "1.1.1.21"
-        }
-
-        dhcp_generic_option {
-          code = "119"
-          values = ["abc"]
-        }
-      }
-    }
-
-    advanced_config {
-      connectivity = "ON"
-    }
-}
-*/
--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,6 @@ output tier0_path {
   value = nsxt_policy_tier0_gateway.this.path
 }
 
+output transport_zone_path {
+  value = data.nsxt_policy_transport_zone.overlay.path
+}
